1eeb67b49fa45fccdbbaaa5be660a07c080f05ed
LukeJohnson23456 <159075383+LukeJohnson23456@users.noreply.github.com>
2025-04-28 00:56:52 -0500

    Update PAYMENT.sql

--- PAYMENT.sql
+++ PAYMENT.sql
@@ -4,7 +4,5 @@ CREATE TABLE PAYMENT (
     Payment_Date    DATE            NOT NULL,
     Method          VARCHAR(10)     NOT NULL,
     Amount          DECIMAL(10,2)   NOT NULL,
-    O_ID            INT,
-    FOREIGN KEY (O_ID) REFERENCES ORDER_(Order_ID),
     PRIMARY KEY(Payment_ID)
 );
