51e73b8e311b58538150cc2402a9b66c58f6389e
LukeJohnson23456 <159075383+LukeJohnson23456@users.noreply.github.com>
2025-04-28 00:54:59 -0500

    Update CUSTOMER.sql

--- CUSTOMER.sql
+++ CUSTOMER.sql
@@ -1,12 +1,12 @@
 CREATE TABLE CUSTOMER (
 
     Customer_ID     INT             NOT NULL,
-    First_name      VARCHAR(15)     NOT NULL,
-    Last_name       VARCHAR(15)     NOT NULL,
+    First_name      VARCHAR(15),
+    Last_name       VARCHAR(15),
     email           VARCHAR(320)    NOT NULL,
     Phone_Number    CHAR(10),
     City            VARCHAR(100),
-    State_          VARCHAR(12),
+    State_          CHAR(2),
     Zip_Code        CHAR(5),
     Street          VARCHAR(100),
     PRIMARY KEY(Customer_ID)
