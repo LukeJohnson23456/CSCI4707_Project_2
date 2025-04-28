4fd5299b374df71d3bd60a69a9a160ba8895452a
LukeJohnson23456 <159075383+LukeJohnson23456@users.noreply.github.com>
2025-04-28 00:54:39 -0500

    Update BOOK_ORDER.sql

--- BOOK_ORDER.sql
+++ BOOK_ORDER.sql
@@ -3,7 +3,7 @@ CREATE TABLE BOOK_ORDER (
     Book_ID         INT             NOT NULL,
     Quantity        INT             NOT NULL,
     Price           DECIMAL(10,2)   NOT NULL,
+    PRIMARY KEY(Order_ID, Book_ID),
     FOREIGN KEY (Order_ID) REFERENCES ORDER_(Order_ID),
-    FOREIGN KEY (Book_ID) REFERENCES BOOK(BookID),
-    PRIMARY KEY(Order_ID, Book_ID)
+    FOREIGN KEY (Book_ID) REFERENCES BOOK(BookID)
 );
