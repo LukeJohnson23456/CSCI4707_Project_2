b0c51d5aa2ec044c71b89dffd4083848994e9f73
LukeJohnson23456 <159075383+LukeJohnson23456@users.noreply.github.com>
2025-04-28 00:56:25 -0500

    Update ORDER_.sql

--- ORDER_.sql
+++ ORDER_.sql
@@ -5,9 +5,11 @@ CREATE TABLE ORDER_ (
     Ship_Date       DATE,
     Status_         VARCHAR(10),
     C_ID            INT             NOT NULL,
+    P_ID            INT             NOT NULL,
     E_ID            INT             NOT NULL,
+    PRIMARY KEY(Order_ID),
     FOREIGN KEY (C_ID) REFERENCES CUSTOMER(Customer_ID),
-    FOREIGN KEY (E_ID) REFERENCES EMPLOYEE(Employee_ID),
-    PRIMARY KEY(Order_ID)
+    FOREIGN KEY (P_ID) REFERENCES PAYMENT(PaymentID),
+    FOREIGN KEY (E_ID) REFERENCES EMPLOYEE(Employee_ID)
 
 );
