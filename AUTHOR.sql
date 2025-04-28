e18991a2acd302ecd0b2b942ea095bbc457777e8
LukeJohnson23456 <159075383+LukeJohnson23456@users.noreply.github.com>
2025-04-27 23:08:11 -0500

    Create AUTHOR.sql

---
+++ AUTHOR.sql
@@ -0,0 +1,8 @@
+CREATE TABLE AUTHOR (
+    AuthorID        INT             NOT NULL,
+    First_name      VARCHAR(15),
+    Last_name       VARCHAR(15), 
+    Bio             VARCHAR(1000),
+    PRIMARY KEY(AuthorID)
+
+);
