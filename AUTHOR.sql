0974e70a4b80074f9a4996470a0145384210073a
LukeJohnson23456 <159075383+LukeJohnson23456@users.noreply.github.com>
2025-04-28 00:53:42 -0500

    Update AUTHOR.sql

--- AUTHOR.sql
+++ AUTHOR.sql
@@ -1,7 +1,7 @@
 CREATE TABLE AUTHOR (
     AuthorID        INT             NOT NULL,
-    First_name      VARCHAR(15),
-    Last_name       VARCHAR(15), 
+    First_name      VARCHAR(15)     NOT NULL,
+    Last_name       VARCHAR(15)     NOT NULL,
     Bio             VARCHAR(1000),
     PRIMARY KEY(AuthorID)
 
