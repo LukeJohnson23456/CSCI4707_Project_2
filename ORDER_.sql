0cf82d090d98930f94828c1d8d069d116549fca0
LukeJohnson23456 <159075383+LukeJohnson23456@users.noreply.github.com>
2025-04-30 00:36:28 -0500

    Update ORDER_.sql

--- ORDER_.sql
+++ ORDER_.sql
@@ -1,6 +1,6 @@
 CREATE TABLE ORDER_ (
     Order_ID        INT             NOT NULL,
-    ShippingAddress VARCHAR(100),
+    ShippingAddress VARCHAR(200),
     Order_Date      DATE            NOT NULL,
     Ship_Date       DATE,
     Status_         VARCHAR(10),
@@ -8,8 +8,10 @@ CREATE TABLE ORDER_ (
     P_ID            INT             NOT NULL,
     E_ID            INT             NOT NULL,
     PRIMARY KEY(Order_ID),
-    FOREIGN KEY (C_ID) REFERENCES CUSTOMER(Customer_ID),
-    FOREIGN KEY (P_ID) REFERENCES PAYMENT(PaymentID),
+    FOREIGN KEY (C_ID) REFERENCES CUSTOMER(Customer_ID)
+        ON DELETE CASCADE   ON UPDATE CASCADE,
+    FOREIGN KEY (P_ID) REFERENCES PAYMENT(PaymentID)
+        ON DELETE CASCADE   ON UPDATE CASCADE,
     FOREIGN KEY (E_ID) REFERENCES EMPLOYEE(Employee_ID)
-
+        ON DELETE CASCADE   ON UPDATE CASCADE
 );
